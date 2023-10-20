447c379da1fa4f2b847d2808aeadce75bd367d6e
Fahid Hasan Fuad <96814607+Fahid24@users.noreply.github.com>
2023-10-20 19:29:33 +0600

    added

---
+++ src/fake-data.js
@@ -0,0 +1,114 @@
+export const surpriceCategories = [
+    {
+        name: "Premium Chocolate Gift Box (Max Collection) 910",
+        price: "6,650",
+        cutPrice: "7,508",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-910-510x510.jpg"
+    },
+    {
+        name: "Chocolate Basket Gift Hamper – 903",
+        price: "5,055",
+        cutPrice: "",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-903-247x247.jpg"
+    },
+    {
+        name: "Premium Chocolate Gift Box (Recommend Collection) 911",
+        price: "4,134",
+        cutPrice: "",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-911-247x247.jpg"
+    },
+    {
+        name: "Chocolate Basket Gift Hamper – 905",
+        price: "4,070",
+        cutPrice: "",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-905-247x247.jpg"
+    },
+    {
+        name: "Premium Chocolate Gift Box (Max Collection) 910",
+        price: "6,650",
+        cutPrice: "7,508",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-902-247x247.jpg"
+    },
+    {
+        name: "Premium Chocolate Gift Box 909",
+        price: "3,179",
+        cutPrice: "",
+        image: "https://www.chococraving.com/wp-content/uploads/2022/12/Premium-chocolate-gift-box-909-247x247.jpg"
+    },
+]
+export const handmadeCategories = [
+    {
+        name: "Explosion Box Gift (CODE: 012)",
+        price: "999",
+        cutPrice: "1300",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/explosion-box-gift-510x510.jpg"
+    },
+    {
+        name: "Explosion Box Gift (CODE: 013)",
+        price: "1,200",
+        cutPrice: "1,030",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/explosion-box-gift-bd-1-510x510.jpg"
+    },
+    {
+        name: "Explosion Box Gift (CODE: 010)",
+        price: "1,050",
+        cutPrice: "1,250 ",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/explosion-box-gift-bangladesh-510x510.jpg"
+    },
+    {
+        name: "Best Explosion Box Chocolate BD (CODE: 008)",
+        price: "1,730",
+        cutPrice: "2,200 ",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/WhatsApp-Image-2023-08-25-at-8.53.20-PM-510x510.jpeg"
+    },
+    {
+        name: "Best Explosion Box Chocolate BD (CODE: 009)",
+        price: "3,150",
+        cutPrice: "3,500 ",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/WhatsApp-Image-2023-08-25-at-8.53.20-PM-2-510x510.jpeg"
+    },
+    {
+        name: "Explosion Box Gift (CODE: 012)",
+        price: "999",
+        cutPrice: "1,200",
+        image: "https://www.chococraving.com/wp-content/uploads/2023/08/explosion-box-gift-510x510.jpg"
+    },
+]
+export const gadgetCategories = [
+    {
+        name: "WIWU Crystal Transparent Wireless Mouse (Grey)",
+        price: "1,450",
+        cutPrice: "1,600 ",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/07/WIWU-Crystal-Transparent-Wireless-Mouse-.jpeg"
+    },
+    {
+        name: "BOYA BY-V10 Wireless Microphone Type-C Devices",
+        price: "2,800",
+        cutPrice: "3,450",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/08/BOYA-BY-V10-Wicrophone-in-BD.webp"
+    },
+    {
+        name: "Ulanzi J12 Wireless Microphone iPhone (With Charging Case)",
+        price: "3,300",
+        cutPrice: "4,200",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/07/Ulanzi-J12-for-iPhone-in-BD.jpeg"
+    },
+    {
+        name: "Xiaomi Enchen Aurora T+ Sonic Electric Toothbrush (White Color)",
+        price: "1,190",
+        cutPrice: "",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/07/ENCHEN-Aurora-T_-Sonic-Electric-Toothbrush.jpg"
+    },
+    {
+        name: "OWS P-Q3 Motion Wireless Earbuds",
+        price: "1,200",
+        cutPrice: "",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/08/OWS-P-Q3-TWS.jpeg"
+    },
+    {
+        name: "Joyroom W050 20W 10000mah Magnetic Wireless Power Bank (Black Color)",
+        price: "2,500",
+        cutPrice: "2,800",
+        image: "https://dropshop.com.bd/wp-content/uploads/2023/07/Joyroom-JR-W050-20W-10000mah-Magnetic-Wireless-Power-Bank-with-Ring-Holder-Price-in-Bangladesh.webp"
+    },
+]
